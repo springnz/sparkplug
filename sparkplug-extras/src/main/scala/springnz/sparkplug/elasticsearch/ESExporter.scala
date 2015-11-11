852f8f61ec1341a718828b05b6a89077ce586c64
Stephen Zoio <stephen@springdom.com>
2015-11-12 12:39:57 +1300

    Pointed tests to elasticsearch-lib

--- sparkplug-extras/src/main/scala/springnz/sparkplug/elasticsearch/ESExporter.scala
+++ sparkplug-extras/src/main/scala/springnz/sparkplug/elasticsearch/ESExporter.scala
@@ -3,6 +3,7 @@ package springnz.sparkplug.elasticsearch
 import org.apache.spark.rdd.RDD
 import org.elasticsearch.spark._
 import springnz.sparkplug.core.SparkOperation
+import springnz.sparkplug.elasticsearch.ESExporter.ESExporterParams
 
 import scala.reflect.ClassTag
 import scala.util.Try
@@ -10,24 +11,30 @@ import scalaz.syntax.bind._
 
 object ESExporter {
   type ESExportResult[K, V] = (RDD[Map[K, V]], Try[Unit])
+
+  case class ESExporterParams(nodes: String = "",
+    port: String = "",
+    idField: String = "",
+    includeFields: String = "",
+    excludeFields: String = "",
+    extraConfig: Map[String, String] = Map.empty)
 }
 
 class ESExporter[K: ClassTag, V: ClassTag](
   resourceIndex: String,
   resourceType: String,
-  idField: String = "",
-  includeFields: String = "",
-  excludeFields: String = "",
-  extraConfig: Map[String, String] = Map.empty)
+  exporterParams: ESExporterParams = ESExporterParams())
     extends Serializable {
 
   import ESExporter._
 
   val configMap: Map[String, String] = Map[String, String](
-    "es.mapping.id" -> idField,
-    "es.mapping.include" -> includeFields,
-    "es.mapping.exclude" -> excludeFields)
-    .filter { case (_, v) ⇒ v.nonEmpty } ++ extraConfig
+    "es.mapping.id" -> exporterParams.idField,
+    "es.nodes" -> exporterParams.nodes,
+    "es.port" -> exporterParams.port,
+    "es.mapping.include" -> exporterParams.includeFields,
+    "es.mapping.exclude" -> exporterParams.excludeFields)
+    .filter { case (_, v) ⇒ v.nonEmpty } ++ exporterParams.extraConfig
 
   def export(dataSource: RDD[Map[K, V]]): SparkOperation[ESExportResult[K, V]] = {
     val savedData: SparkOperation[Try[Unit]] = SparkOperation { _ ⇒
@@ -41,3 +48,4 @@ class ESExporter[K: ClassTag, V: ClassTag](
     (dataSourceSparkOperation ⊛ savedData).tupled
   }
 }
+
