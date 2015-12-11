e9d9416057d8096f081691d90d232b8ed994464f
Stephen Zoio <stephen@springdom.com>
2015-12-11 17:57:33 +1300

    Updated utillib 2.8.0 eslib 0.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
   val sparkVersion = "1.5.1"
   val akkaVersion = "2.3.12"
   val elasticsearchVersion = "2.0.0"
-  val utilLibVersion = "2.7.0"
-  val elasticsearchLibVersion = "0.1.1-SNAPSHOT"
+  val utilLibVersion = "2.8.0"
+  val elasticsearchLibVersion = "0.1.1"
 
   val cassandraConnectorVersionMap = Map(
     "1.5.1" -> "1.5.0-M2",
