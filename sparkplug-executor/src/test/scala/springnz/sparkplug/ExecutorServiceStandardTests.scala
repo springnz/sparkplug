5359dda001d93eb968141cdb19a9e9f1018bafe4
Stephen Zoio <stephenzoio@yahoo.com>
2015-12-17 10:01:20 +1300

    Fixed random deathwatch test failures

--- sparkplug-executor/src/test/scala/springnz/sparkplug/ExecutorServiceStandardTests.scala
+++ sparkplug-executor/src/test/scala/springnz/sparkplug/ExecutorServiceStandardTests.scala
@@ -8,6 +8,7 @@ import springnz.sparkplug.core.{ SparkPlugException, Configurer, LocalConfigurer
 import springnz.sparkplug.executor.MessageTypes._
 import springnz.sparkplug.executor.{ Constants, ExecutorService }
 
+import scala.concurrent._
 import scala.concurrent.duration._
 
 class ExecutorServiceStandardTests(_system: ActorSystem)
@@ -15,34 +16,36 @@ class ExecutorServiceStandardTests(_system: ActorSystem)
 
   case object ServerTerminated
 
-  def this() = this(ActorSystem("TestSystem", ConfigFactory.load().getConfig(Constants.defaultConfigSectionName)))
+  def this() = this(ActorSystem("TestSystemStandard", ConfigFactory.load().getConfig(Constants.defaultConfigSectionName)))
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
   "notify the client that server is ready" in new ExecutorServiceFixture(self, "client1", "testBroker1") {
-    expectMsg(1 seconds, ServerReady)
+    expectMsg(3 seconds, ServerReady)
   }
 
   "successfuly execute a job request via a plugin" in new ExecutorServiceFixture(self, "client2", "testBroker2") {
-    expectMsg(1 seconds, ServerReady)
     val requestBroker = system.actorSelection(s"/user/testBroker2")
     val request = JobRequest("springnz.sparkplug.examples.LetterCountPlugin", None)
+    Await.ready(readyPromise.future, 3 seconds)
     requestBroker ! request
-    expectMsg[JobSuccess](6 seconds, JobSuccess(request, (2, 2)))
+    expectMsg(3 seconds, ServerReady)
+    expectMsg[JobSuccess](20 seconds, JobSuccess(request, (2, 2)))
   }
 
   "deathwatch on job processor should produce jobfailed message" in new ExecutorServiceFixture(self, "client3", "testBroker3") {
-    expectMsg(1 seconds, ServerReady)
     import scala.concurrent._
     val requestBroker = system.actorSelection(s"/user/testBroker3")
     // give it something to do for a while
     val request = JobRequest("springnz.sparkplug.examples.WaitPlugin", Some(6000))
+    Await.ready(readyPromise.future, 3 seconds)
     requestBroker ! request
     blocking { Thread.sleep(1000) }
     system.actorSelection(s"/user/testBroker3/jobProcessor-0") ! PoisonPill
     blocking { Thread.sleep(1000) }
+    expectMsg(3 seconds, ServerReady)
     expectMsgType[JobFailure](3 seconds)
   }
 }
