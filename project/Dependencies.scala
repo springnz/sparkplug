879a67d75f336184b913aa60c5197c12f7ec18da
Stephen Zoio <stephen@springdom.com>
2016-02-05 11:18:02 +1300

    Override spark version (currently to 1.5.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -105,6 +105,12 @@ object Dependencies {
   val jacksonOverride = "com.fasterxml.jackson.core" % "jackson-databind" % "2.4.4"
   val guavaOverride = "com.google.guava" % "guava" % "18.0"
 
-  val dependencyOverridesSet = Set(jacksonOverride, guavaOverride)
+  val sparkOverrides = Set(
+    "org.apache.spark" %% "spark-core" % sparkVersion,
+    "org.apache.spark" %% "spark-sql" % sparkVersion,
+    "org.apache.spark" %% "spark-launcher" % sparkVersion
+  )
+
+  val dependencyOverridesSet = Set(jacksonOverride, guavaOverride) ++ sparkOverrides
 }
 
