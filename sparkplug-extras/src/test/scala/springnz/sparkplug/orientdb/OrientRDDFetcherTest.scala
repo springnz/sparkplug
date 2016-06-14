73093963eed4eb0962e456a8a1e0f15fda95e790
Mike Ourednik <mike@spring.co.nz>
2016-06-14 15:20:24 +1200

    Improved test of sql projection

--- sparkplug-extras/src/test/scala/springnz/sparkplug/orientdb/OrientRDDFetcherTest.scala
+++ sparkplug-extras/src/test/scala/springnz/sparkplug/orientdb/OrientRDDFetcherTest.scala
@@ -24,6 +24,7 @@ class OrientRDDFetcherTest extends WordSpec with ShouldMatchers with ODBMemoryTe
       def newDoc(id: Int) = {
         val doc = new ODocument(className)
         doc.field("id", id)
+        doc.field("value", id)
         doc.save()
       }
       log.info(s"creating class $className ...")
@@ -34,7 +35,7 @@ class OrientRDDFetcherTest extends WordSpec with ShouldMatchers with ODBMemoryTe
 
     // select * from className
     val resultAll = execute(
-      OrientRDDFetcher.select(className, Some("id"), None, OrientRDDFetcherTest.docTransformer)
+      OrientRDDFetcher.select(className, None, None, OrientRDDFetcherTest.docTransformer)
         .map(_.collect().toList)).get
 
     resultAll shouldBe (1 to numDocs).toList
@@ -45,6 +46,18 @@ class OrientRDDFetcherTest extends WordSpec with ShouldMatchers with ODBMemoryTe
         .map(_.collect().toList)).get
 
     resultWithWhereClause shouldBe (1 to numDocs / 2).toList
+
+    // select id from className
+    val fieldNames1 = execute(
+      OrientRDDFetcher.select(className, Some("id"), None, doc ⇒ doc.fieldNames().toList)
+        .map(_.collect())).get.flatten.toSet
+    fieldNames1 shouldBe Set("id")
+
+    // select id, value from className
+    val fieldNames2 = execute(
+      OrientRDDFetcher.select(className, Some("id, value"), None, doc ⇒ doc.fieldNames().toList)
+        .map(_.collect())).get.flatten.toSet
+    fieldNames2 shouldBe Set("id", "value")
   }
 
   "Save SparkRDD to OrientDB" in new SimpleTestContext("") {
@@ -65,5 +78,7 @@ class OrientRDDFetcherTest extends WordSpec with ShouldMatchers with ODBMemoryTe
 
 object OrientRDDFetcherTest {
   case class Dictator(year: Int, name: String, country: String)
-  def docTransformer(doc: ODocument) = doc.field("id").asInstanceOf[Int]
+  def docTransformer(doc: ODocument) = {
+    doc.field("id").asInstanceOf[Int]
+  }
 }
